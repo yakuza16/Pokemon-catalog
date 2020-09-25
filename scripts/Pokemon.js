510ff6b47ee6171701d8508b3ecefed5fba79750
Marcin Spławiński <64729676+yakuza16@users.noreply.github.com>
2020-09-25 20:37:56 +0200

    trying to fix fetch for change cards

--- scripts/Pokemon.js
+++ scripts/Pokemon.js
@@ -5,7 +5,7 @@ class Pokemon {
         this.pokemonCatalog = null;
         this.loadMoreButton = null;
         this.page = 1;
-        this.pageSize = 2;
+        this.pageSize = 1;
         this.version = 'v1';
 
         this.UIIdis = {
@@ -14,14 +14,17 @@ class Pokemon {
             loaderId: 'js-loader',
         }
 
+        this.pulledCards = [];
         this.cardsCollection = [];
 
-        this.API = `https://api.pokemontcg.io/v1/cards/?page=${this.page}&pageSize=${this.pageSize}`;
+        this.API = `https://api.pokemontcg.io/${this.version}/cards/?page=${this.page}&pageSize=${this.pageSize}`;
     }
 
     initializeApp() {
         this.bindElements();
-        this.pullCards()
+        this.pullCards();
+        this.loadMoreButton.addEventListener('click', () => this.pullCards());
+
     }
 
     bindElements() {
@@ -31,57 +34,65 @@ class Pokemon {
     };
 
     async pullCards() {
+        this.page++;
+        this.loader.classList.toggle('loader--is-visible');
+
         const {
             cards
         } = await this.fetchData(this.API);
-        console.log(cards)
-        this.cardsCollection = [...cards];
+        console.log(cards);
+        this.loader.classList.toggle('loader--is-visible');
+        this.pulledCards = [...cards]
+        this.cardsCollection = [...this.pulledCards];
         console.log(this.cardsCollection);
-        this.cardsCollection.forEach(card =>
-            this.displayPokemon()
-        )
+        this.cardsCollection.forEach((card, index) => {
+            this.displayPokemon(index);
+        })
+
+
+
     }
 
     async fetchData(URL) {
         try {
             const response = await fetch(URL);
             const parsedResponse = await response.json();
-            console.log(parsedResponse)
+            console.log(parsedResponse);
             return parsedResponse;
         } catch (error) {
             console.log(error);
             return;
         }
-
     }
 
-    displayPokemon() {
+    displayPokemon(index) {
         const pokemonCard = document.createElement('div');
         pokemonCard.classList.add('card');
         pokemonCard.innerHTML = `
         
         <div class="card__upper-infos">
-        <h6 class="card__title" id="js-cardTitle">${this.cardsCollection.name}</h6>
-        <p class="card__number">Nr: <span class="card__number-id" id="js-cardNumber">55</span></p>
+        <h6 class="card__title" id="js-cardTitle">${this.cardsCollection[index].name}</h6>
+        <p class="card__number">Nr: <span class="card__number-id" id="js-cardNumber">${this.cardsCollection[index].number}</span></p>
     </div>
 
 
     
     <div class="card__image-container">
-        <img src="" alt="" class="card__image" id="js-cardImage">
+        <img src="${this.cardsCollection[index].imageUrl}" alt="" class="card__image" id="js-cardImage">
     </div>
 
 
     <div class="card__bottom-infos">
-        <p class="card__supertype" id="js-supertypeInfo">Pokemon</p>
-        <p class="card__subtype" id="js-subtypeInfo">Stage 1</p>
-        <p class="card__rarity" id="jsrarityInfo">Uncommon</p>
+        <p class="card__supertype" id="js-supertypeInfo">${this.cardsCollection[index].supertype}</p>
+        <p class="card__subtype" id="js-subtypeInfo">${this.cardsCollection[index].subtype}</p>
+        <p class="card__rarity" id="jsrarityInfo">${this.cardsCollection[index].rarity}</p>
     </div>
         
         
         `;
 
         this.pokemonCatalog.appendChild(pokemonCard);
+        this.page++;
 
     }
 
